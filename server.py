49bf3b3cea69a60cb7ab7320d770cd7809cb6365
Adam Braccia <111795014+abraccia@users.noreply.github.com>
2025-11-05 12:32:05 -0500

    basic echo

---
+++ server.py
@@ -0,0 +1,21 @@
+import socket
+
+TARGET_IP = input("Enter your target IP Address")
+TARGET_PORT = input("Enter your target Port")
+
+HOST = "127.0.0.1"
+PORT = 65432
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        print(f"Connected by {addr}")
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            conn.sendall(data)
+
+
